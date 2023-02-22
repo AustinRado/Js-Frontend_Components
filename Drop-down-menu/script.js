69a48571c430de0efca4532b571650ac0ca61f5f
AustinRado <radoaussy@gmail.com>
2023-02-22 13:57:05 +0300

    functionality

--- Drop-down-menu/script.js
+++ Drop-down-menu/script.js
@@ -1,26 +1,17 @@
-const HOST = 'server.com/';
-
-document.onclick = function() {
-  api.get(HOST, {}, displayText);
-}
-
-function displayText(response) {
-  document.body.innerHTML += response;
-}
-
-function menuItemEnter(){
+function showSubmenu() {
     const submenu = document.getElementsByClassName("menu__sub")[0];
-   
     submenu.style.display = "block";
-}
-//document.getElementsByClassName will return a list of elements with the given class name. Even if there is only one element with that class name it will be in a Node List which is why you have to use the [0]
-function menuItemLeave(){
+  }
+  
+  function hideSubmenu() {
     const submenu = document.getElementsByClassName("menu__sub")[0];
-   
     submenu.style.display = "none";
-}
-const menuItems = document.getElementsByClassName("menu__main__item");
-for (const menuItem of menuItems){
-    menuItem.onmouseenter = menuItemEnter;
-    menuItem.onmouseleave = menuItemLeave;
-}
+  }
+  
+  const menuItems = document.getElementsByClassName("menu__main__item");
+  for (const menuItem of menuItems) {
+    menuItem.onmouseenter = showSubmenu
+  }
+  
+  const menu = document.getElementsByClassName("menu")[0];
+  menu.onmouseleave = hideSubmenu
